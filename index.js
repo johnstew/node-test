32dce64fa4340914aa7276d196dde56cf764d094
jstewart <johnathan.stewart@mlb.com>
2018-07-24 11:25:56 -0400

    Simple express test

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Up and running on PORT:${PORT}`))
